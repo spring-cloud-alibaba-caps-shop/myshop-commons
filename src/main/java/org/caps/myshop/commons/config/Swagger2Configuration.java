ff82fef4260afe53b113196d53f37b385c66e478
caps <279205343@qq.com>
2019-06-02 22:37:13 +0800

    1

---
+++ src/main/java/org/caps/myshop/commons/config/Swagger2Configuration.java
@@ -0,0 +1,35 @@
+package org.caps.myshop.commons.config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+/**
+ * @author caps
+ * @Date 2019/5/27 17:33
+ * @Description
+ */
+@Configuration
+public class Swagger2Configuration {
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("org.caps.myshop.service"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("MyShop API 文档")
+                .description("MyShop API 网关接口，http://www.funtl.com")
+                .termsOfServiceUrl("http://www.funtl.com")
+                .version("1.0.0")
+                .build();
+    }
+}
